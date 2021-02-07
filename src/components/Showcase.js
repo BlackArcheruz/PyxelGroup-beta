95d3feec2fbe9858bc43c0f4bacc4719defbcc17
Muhammad Siddiq Ibragimov <67962548+MuhammadSiddiq-pyxel@users.noreply.github.com>
2021-02-07 22:14:31 +0500

    update

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,6 +1,7 @@
 import AOS from 'aos';
 import 'aos/dist/aos.css'
 import {useEffect} from 'react';
+import {Link} from 'react-router-dom';
 
 const Showcase = () => {
     useEffect(()=>{
@@ -12,8 +13,8 @@ const Showcase = () => {
             <h3 style={{textAlign:'center'}}>WE'RE</h3>
             <h1>CREATIVE <br/> AGENCY</h1>
             <p>Developing high-end websites, mobile applications and chatbots which will stand out among your competitors. Designing a unique UI with the best user experience, making your clients enjoy your service</p>
-            <button className="btn btn-light">Learn More</button>
-            <button className="btn btn-outline">Buy Now</button>
+            <Link to='/services'><button className="btn btn-light">Learn More</button></Link>
+            <Link to='/contact'><button className="btn btn-outline">Buy Now</button></Link>
         </div>
     )
 }
