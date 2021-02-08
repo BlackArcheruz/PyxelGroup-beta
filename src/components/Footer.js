9a990555fe829671b583dad3a14f4bd3f2087b5e
Muhammad Siddiq Ibragimov <67962548+MuhammadSiddiq-pyxel@users.noreply.github.com>
2021-02-08 20:43:26 +0500

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,7 @@
-import logo from '../components/images/favicon.png'
-import {Link} from 'react-router-dom'
-
 const Footer = () => {
     return (
         <footer> 
-            <ul>
-                <li><a href="/" style={{fontSize: 'xx-large'}}><img src={logo} alt="" style={{width: '45px'}}/>yxelGroup</a></li>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/services">Services</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-                <li><a href="https://github.com/pyxel-group"><i className="fab fa-github"></i></a></li>
-                <li><a href="https://www.linkedin.com/company/pyxel-group"><i className="fab fa-linkedin"></i></a></li>
-            </ul>
+            PyxelGroup Copyright &copy; 2021
         </footer>
     )
 }
